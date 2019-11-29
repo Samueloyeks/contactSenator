02168a914af6419a53c7328d6e0dc3c573d5f410
Sammy Seyi <sammyoyekeye70@gmail.com>
2019-11-29 09:38:30 +0100

    update

--- server.js
+++ server.js
@@ -15,13 +15,13 @@
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
 
-var express = require('express');
-var path = require('path');
-var app = express();
+// var express = require('express');
+// var path = require('path');
+// var app = express();
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get('/', function (req, res) {
+// 	res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 3000);
+// app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+// app.listen(process.env.PORT || 3000);
